51f05c9557569b6ccf1e1abe04da28dcc9c2f8d4
AzamHasnat <hasnat.azam@g.bracu.ac.bd>
2024-02-16 22:25:13 +0600

    Start working on fade transation

---
+++ lib/Widgests of the week/fade_transation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FadeTransationWidget extends StatefulWidget {
+  const FadeTransationWidget({super.key});
+
+  @override
+  State<FadeTransationWidget> createState() => _FadeTransationWidgetState();
+}
+
+class _FadeTransationWidgetState extends State<FadeTransationWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
